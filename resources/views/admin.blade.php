e24925999894ee3c60f69e6a934336041972b6b9
Jagad Wijaya Purnomo <Jagadpurnomo26@gmail.com>
2020-11-14 16:23:59 +0700

    Initial commit

---
+++ resources/views/admin.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Selamat datang di dashboard Admin</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    Tolong jangan di hack jika bukan admin 
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
